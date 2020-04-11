8720ca426ff00d9b886486741e741b2047631dcb
axmty <alexandre.metgy@gmail.com>
2020-04-11 13:41:56 +0200

    Call AccuWeather Current Conditions API.

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,15 +1,34 @@
-const apiKey = 'UjGZ3wDnAgxo5dfepqdFeA5ClutLSTQe';
+const apiKey = 'UjGZ3wDnAgxo5dfepqdFeA5ClutLSTQe  zzazs';
 
-const getCity = async city => {
-  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-  const query = `?apikey=${apiKey}&q=${city}`;
+const buildQueryString = queryParams => {
+  queryParams = queryParams ?? [];
+  queryParams.push({ key: 'apikey', value: apiKey });
+
+  return '?' + queryParams
+    .map(param => `${param.key}=${param.value}`)
+    .join('&');
+};
+
+const getCityCode = async city => await fetchApi(
+  'locations/v1/cities/search',
+  data => data[0],
+  [{ key: 'q', value: city }]);
+
+const getWeather = async cityKey => await fetchApi(
+  `currentconditions/v1/${cityKey}`,
+  data => data[0]);
+
+const fetchApi = async (path, dataSelector, queryParams) => {
+  const base = 'http://dataservice.accuweather.com/' + path;
+  const query = buildQueryString(queryParams);
 
   const response = await fetch(base + query);
   const data = await response.json();
 
-  return data[0];
+  return dataSelector(data);
 };
 
-getCity('manchester')
+getCityCode('manchester')
+  .then(data => getWeather(data.Key))
   .then(data => console.log(data))
   .catch(err => console.log(err));
