8f4e81a9620720ea89f96c8b07a9f4804c92309f
axmty <alexandre.metgy@gmail.com>
2020-04-11 13:58:05 +0200

    Fix updateCity function to getCityInformation and use object shorthand notation.

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,10 @@
 const cityForm = document.querySelector('form.change-location');
 
-const updateCity = async city => {
+const getCityInformation = async city => {
   const cityDetails = await getCity(city);
   const weather = await getWeather(cityDetails.Key);
 
-  return {
-    cityDetails: cityDetails,
-    weather: weather
-  };
+  return { cityDetails, weather };
 };
 
 cityForm.addEventListener('submit', e => {
@@ -16,7 +13,7 @@ cityForm.addEventListener('submit', e => {
   const city = cityForm.city.value.trim();
   cityForm.reset();
 
-  updateCity(city)
+  getCityInformation(city)
     .then(data => console.log(data))
     .catch(err => console.log(err));
 });
